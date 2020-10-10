daa1053810aa64eacbd553652e44659f801d3d45
horler408 <horler.web@gmail.com>
2020-10-10 21:50:19 +0100

    Validating form input

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 const sideToggle = document.querySelector('.head__sidebar-toggle')
 const closeBtn = document.querySelector('.close-btn');
 const aside = document.querySelector('.aside');
+const errorMsg = document.querySelector('.error-message')
+const input = document.querySelector('.url-input')
+const form = document.querySelector('.url_form')
 // const login = document.querySelector('.login')
 // const signUp = document.querySelector('.signup')
 // const formBtn = document.querySelector('.form__buttons')
+
+
 
 const url = `https://rel.ink/api/links/Nn8y9p/`;
 //POSTurl = "url": "https://news.ycombinator.com/"
@@ -23,5 +28,23 @@ const getRequest = async () => {
   console.log(data);
 };
 
-getRequest();
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  if(input.value === '') {
+    errorMsg.textContent = '*Please enter a valid url';
+    input.classList.add('error');
+    return
+  }else {
+    errorMsg.textContent = '';
+    input.classList.remove('error');
+
+    console.log(input.value);
+    getRequest();
+
+    input.value = '';
+  }
+
+  
+})
+
 
