db7afa7cdd68987c72235ea6d877ad2d6c5f5cef
horler408 <horler.web@gmail.com>
2020-10-10 16:20:53 +0100

    Completing sidebar effects

--- js/app.js
+++ js/app.js
@@ -1,13 +1,21 @@
-const sideToggle = document.querySelector('sidebar__toggle')
-const closeBtn = document.querySelector('.close-btn')
-const aside = document.querySelector('.aside')
+const sideToggle = document.querySelector('.head__sidebar-toggle')
+const closeBtn = document.querySelector('.close-btn');
+const aside = document.querySelector('.aside');
+// const login = document.querySelector('.login')
+// const signUp = document.querySelector('.signup')
+// const formBtn = document.querySelector('.form__buttons')
 
 const url = `https://rel.ink/api/links/Nn8y9p/`;
 //POSTurl = "url": "https://news.ycombinator.com/"
 
 sideToggle.addEventListener('click', () => {
-  aside.classList.toggle()
+  aside.classList.toggle('show-sidebar')
 })
+
+closeBtn.addEventListener('click', () => {
+  aside.classList.remove('show-sidebar')
+})
+
 
 const getRequest = async () => {
   const response = await fetch(url);
