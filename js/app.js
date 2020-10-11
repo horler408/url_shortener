10c77390bc340806814d3b5a242edd94386d5fc6
horler408 <horler.web@gmail.com>
2020-10-11 22:49:12 +0100

    Adding dummy shorten result

--- js/app.js
+++ js/app.js
@@ -4,10 +4,7 @@ const aside = document.querySelector('.aside');
 const errorMsg = document.querySelector('.error-message')
 const input = document.querySelector('.url-input')
 const form = document.querySelector('.url_form')
-// const login = document.querySelector('.login')
-// const signUp = document.querySelector('.signup')
-// const formBtn = document.querySelector('.form__buttons')
-
+const resultContainer = document.querySelector('.result__container')
 
 
 //const url = `https://rel.ink/api/links/Nn8y9p/`;
@@ -21,6 +18,35 @@ closeBtn.addEventListener('click', () => {
   aside.classList.remove('show-sidebar')
 })
 
+function append(parent, el) {
+  return parent.appendChild(el); 
+}
+
+const createResult = () => {
+  let result = document.createElement('div')
+  let url = document.createElement('span')
+  let link = document.createElement('span')
+  let anchor = document.createElement('a')
+  let copyBtn = document.createElement('button')
+
+  url.setAttribute('class', 'left_url')
+  link.setAttribute('class', 'right_url')
+  anchor.setAttribute('href', '#')
+  copyBtn.setAttribute('class', 'copy_button')
+  copyBtn.setAttribute('type', 'button')
+  result.setAttribute('class', 'result')
+
+  url.textContent = input.value
+  link.innerHTML = `https://rel.ink/k4lKyk`;
+  copyBtn.innerHTML = `copy`
+  
+
+  append(link, anchor)
+  append(result, link)
+  append(result, url)
+  append(result, copyBtn)
+  append(resultContainer, result)
+}
 
 const getRequest = async (link) => {
   const url = `https://rel.ink/api/${link}/Nn8y9p/`;
@@ -29,18 +55,21 @@ const getRequest = async (link) => {
   console.log(data);
 };
 
+const regex = 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)'
+const urlMatch = input.value.match(regex)
+
 form.addEventListener('submit', (e) => {
   e.preventDefault()
-  if(input.value === '') {
-    errorMsg.textContent = '*Please enter a valid url';
+  if(input.value === '' && !urlMatch) {
+    errorMsg.textContent = '*Please enter a valid url e.g https://www.google.com';
     input.classList.add('error');
     return
   }else {
     errorMsg.textContent = '';
     input.classList.remove('error');
 
-    //console.log(input.value);
-    getRequest(input.value);
+    //getRequest(input.value);
+    createResult()
 
     input.value = '';
   }
---
+++ js/regex.js
@@ -0,0 +1,14 @@
+var expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+var regex = new RegExp(expression);
+var t = 'www.google.com';
+
+if (t.match(regex)) {
+  alert("Successful match");
+} else {
+  alert("No match");
+}
+
+//Http_Regex: 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)'
+//[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)
+
+//(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})
